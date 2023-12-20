e5add5affea3afc75dab476bfbb8c11be427b156
sClarkeDevz <sclarkedev@gmail.com>
2023-12-20 14:26:10 +0000

    v1.0.0 Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# binge-hub-proxy
+
+Simple reverse proxy to bypass CORS, used by BingeHub.
+
+### features:
+ - Deployable on many platforms - thanks to nitro
+ - header rewrites - read and write protected headers
+ - bypass CORS - always allows browser to send requests through it
+ - Supabase auth using JWT Verification
+
+### supported platforms:
+ - cloudflare workers
+ - AWS lambda
+ - nodejs
---
+++ src/routes/index.ts
@@ -0,0 +1,59 @@
+import { isJwtValid } from '@/utils/auth';
+import { getBodyBuffer } from '@/utils/body';
+import { getProxyHeaders, getAfterResponseHeaders, cleanupHeadersBeforeProxy } from '@/utils/headers';
+
+export default defineEventHandler(async (event) => {
+  // grab token from url.
+  const token = getQuery<{ token?: string }>(event).token;
+  if (!token)
+    return await sendJson({
+      event,
+      status: 401,
+      data: {
+        error: 'Unauthorized',
+      },
+    });
+
+  // check the jwt against the supabase secret to see if it is valid
+  const isValid = await isJwtValid(token);
+  if (!isValid) {
+    return await sendJson({
+      event,
+      status: 401,
+      data: {
+        error: 'Unauthorized',
+      },
+    });
+  }
+
+  // handle cors, if applicable
+  if (isPreflightRequest(event)) return handleCors(event, {});
+
+  // parse destination URL
+  const destination = getQuery<{ destination?: string }>(event).destination;
+  if (!destination)
+    return await sendJson({
+      event,
+      status: 400,
+      data: {
+        error: 'destination query parameter invalid',
+      },
+    });
+
+  // read body
+  const body = await getBodyBuffer(event);
+
+  // proxy
+  cleanupHeadersBeforeProxy(event);
+  await proxyRequest(event, destination, {
+    fetchOptions: {
+      redirect: 'follow',
+      headers: getProxyHeaders(event.headers),
+      body,
+    },
+    onResponse(outputEvent, response) {
+      const headers = getAfterResponseHeaders(response.headers, response.url);
+      setResponseHeaders(outputEvent, headers);
+    },
+  });
+});
