05e3323f398d968ed41b4c172efc540f5ba0535f
sClarkeDevz <sclarkedev@gmail.com>
2024-01-07 02:03:20 +0000

    Removed auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,30 +1,30 @@
-import { isJwtValid } from '@/utils/auth';
+// import { isJwtValid } from '@/utils/auth';
 import { getBodyBuffer } from '@/utils/body';
 import { getProxyHeaders, getAfterResponseHeaders, cleanupHeadersBeforeProxy } from '@/utils/headers';
 
 export default defineEventHandler(async (event) => {
   // grab token from url.
-  const token = getQuery<{ token?: string }>(event).token;
-  if (!token)
-    return await sendJson({
-      event,
-      status: 401,
-      data: {
-        error: 'Unauthorized',
-      },
-    });
+  // const token = getQuery<{ token?: string }>(event).token;
+  // if (!token)
+  //   return await sendJson({
+  //     event,
+  //     status: 401,
+  //     data: {
+  //       error: 'Unauthorized',
+  //     },
+  //   });
 
   // check the jwt against the supabase secret to see if it is valid
-  const isValid = await isJwtValid(token);
-  if (!isValid) {
-    return await sendJson({
-      event,
-      status: 401,
-      data: {
-        error: 'Unauthorized',
-      },
-    });
-  }
+  // const isValid = await isJwtValid(token);
+  // if (!isValid) {
+  //   return await sendJson({
+  //     event,
+  //     status: 401,
+  //     data: {
+  //       error: 'Unauthorized',
+  //     },
+  //   });
+  // }
 
   // handle cors, if applicable
   if (isPreflightRequest(event)) return handleCors(event, {});
