34a7eecd7ff2b4b7392b4123b7c0ec6b67a30f68
Sam Clarke <143657751+sClarkeDev@users.noreply.github.com>
2024-05-28 15:16:50 +0100

    Revert "remove auth"
    This reverts commit 43d5858b3a559944d0b020dd2358d83d9d3dbee2.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,29 @@ import { cleanupHeadersBeforeProxy, getAfterResponseHeaders, getProxyHeaders } f
 
 
 export default defineEventHandler(async (event) => {
+  // grab token from url.
+  const token = getQuery<{ binge_hub_token?: string }>(event).binge_hub_token;
+  if (!token)
+    return await sendJson({
+      event,
+      status: 401,
+      data: {
+        error: 'Unauthorized',
+      },
+    });
+
+  // check the jwt against the supabase secret to see if it is valid
+  const isValid = token === process.env.token
+  if (!isValid) {
+    return await sendJson({
+      event,
+      status: 401,
+      data: {
+        error: 'Unauthorized',
+      },
+    });
+  }
+
   // handle cors, if applicable
   if (isPreflightRequest(event)) return handleCors(event, {});
 
