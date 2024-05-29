7ffd391109fd60b816a5f0dec42e8520225d46a0
Sam Clarke <143657751+sClarkeDev@users.noreply.github.com>
2024-05-29 17:07:01 +0100

    Removed auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,32 +1,8 @@
-// import { isJwtValid } from '@/utils/auth';
 import { getBodyBuffer } from '@/utils/body';
 import { cleanupHeadersBeforeProxy, getAfterResponseHeaders, getProxyHeaders } from '@/utils/headers';
 
 
 export default defineEventHandler(async (event) => {
-  // grab token from url.
-  const token = getQuery<{ binge_hub_token?: string }>(event).binge_hub_token;
-  if (!token)
-    return await sendJson({
-      event,
-      status: 401,
-      data: {
-        error: 'Unauthorized',
-      },
-    });
-
-  // check the jwt against the supabase secret to see if it is valid
-  const isValid = token === process.env.token
-  if (!isValid) {
-    return await sendJson({
-      event,
-      status: 401,
-      data: {
-        error: 'Unauthorized',
-      },
-    });
-  }
-
   // handle cors, if applicable
   if (isPreflightRequest(event)) return handleCors(event, {});
 
